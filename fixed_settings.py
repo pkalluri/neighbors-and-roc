b94c22c8676e94f779d9587308b404196fb82640
Pratyusha Kalluri <pkalluri@stanford.edu>
2018-03-05 11:25:02 -0800

    Code to get ROC first sentences' sentiments using CoreNLP.

--- fixed_settings.py
+++ fixed_settings.py
@@ -2,6 +2,8 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DATA_ROOT = os.path.join(PROJECT_ROOT, '..', 'data')
+GENERATED_DATA_ROOT = os.path.join(PROJECT_ROOT, 'generated_data')
+
 # OUT_ROOT = os.path.join(PROJECT_ROOT, 'out')
 
 # BLUE_TAG = '\033[94m'
--- scripts/get_first_sentences_sentiments.py
+++ scripts/get_first_sentences_sentiments.py
@@ -1,30 +1,34 @@
-from demo_effect_of_neighbors import util_ROC
+from demo_effect_of_neighbors import util_ROC, fixed_settings
 from demo_effect_of_neighbors.options import Data_Defaults
 
 from subprocess import Popen, PIPE
 import random
-
-# Setup
+import os
 
 # Settings
 data_opts = Data_Defaults()
 data_opts.NUM_STORIES = 3
 
-# Get X
+# Sentences
 stories, contexts, completions = util_ROC.get_stories_contexts_and_completions(data_opts.ROC_FILEPATH, num_stories=data_opts.NUM_STORIES)
 first_sentences = [context[0] for context in contexts]
-print(first_sentences)
 
-# Get Y
+# Sentiment
 proc = Popen('java -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -stdin', shell=True, stdin=PIPE, stdout=PIPE)
 out,err = proc.communicate('\n'.join(first_sentences).encode('utf-8'))
 sentiment_labels = out.decode('utf-8').split()
-simple_positive_sentences = ['That felt good.', 'That was great.']
-simple_negative_sentences = ['That felt bad.', 'That was terrible.']
-simple_next_sentences =[random.choice(simple_positive_sentences) if sentiment_label=='Positive'
-                        else random.choice(simple_negative_sentences)
-                        for sentiment_label in sentiment_labels]
-print(zip(first_sentences, simple_next_sentences))
+
+# Write
+with open(os.path.join(fixed_settings.GENERATED_DATA_ROOT,'first_sentences_sentiments.txt'), 'w') as f:
+    f.write('\n'.join(sentiment_labels))
+    f.close()
+
+# simple_positive_sentences = ['That felt good.', 'That was great.']
+# simple_negative_sentences = ['That felt bad.', 'That was terrible.']
+# simple_next_sentences =[random.choice(simple_positive_sentences) if sentiment_label=='Positive'
+#                         else random.choice(simple_negative_sentences)
+#                         for sentiment_label in sentiment_labels]
+# print(zip(first_sentences, simple_next_sentences))
 
 # from httplib2 import Http
 # from urllib.parse import urlencode
