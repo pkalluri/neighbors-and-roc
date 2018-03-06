a6e59ec1f10754c52cdb52c99babe622bc5c7da2
Pratyusha Kalluri <pkalluri@stanford.edu>
2018-03-06 11:55:17 -0800

    Output activation changed from sigmoid to softmax. Net should choose 1.

--- my_models.py
+++ my_models.py
@@ -1,5 +1,6 @@
-from keras.layers import Input, Embedding, Dense, Activation, Flatten, LSTM
+from keras.layers import Input, Embedding, Dense, Activation, Flatten, LSTM,Dropout
 from keras.models import Model, Sequential
+from keras import regularizers
 
 def model(input_length, num_classes, embedding_matrix_shape=None, embedding_matrix=None, hidden_layers=[]):
     # Create embedding layer
@@ -8,32 +9,27 @@ def model(input_length, num_classes, embedding_matrix_shape=None, embedding_matr
         embedding_layer = Embedding(VOCABULARY_SIZE,
                                     EMBEDDING_SIZE,
                                     input_length=input_length,
-                                    trainable=True)
+                                    trainable=False)
     else:
         (VOCABULARY_SIZE, EMBEDDING_SIZE) = embedding_matrix.shape
         embedding_layer = Embedding(VOCABULARY_SIZE,
                                 EMBEDDING_SIZE,
                                 weights=[embedding_matrix],
                                 input_length=input_length,
-                                trainable=True)
+                                trainable=False)
     # Create model
     model = Sequential()
     model.add(Embedding(VOCABULARY_SIZE, EMBEDDING_SIZE, input_length=input_length))
-    model.add(LSTM(hidden_layers[0], activation='relu'))
-    for size in hidden_layers[1:]:
-        model.add(Dense(size,activation='relu'))
-    model.add(Dense(num_classes, activation='sigmoid'))
-    print(model.summary())
+    model.add(Dropout(.5))
+    if(len(hidden_layers)==0):
+        model.add(LSTM(num_classes, activation='softmax'))
+                       # kernel_regularizer=regularizers.l2(0.01), activity_regularizer=regularizers.l1(.01)))
+    else:
+        model.add(LSTM(hidden_layers[0], activation='tanh'))
+        model.add(Dropout(.5))
+        for size in hidden_layers[1:]:
+            model.add(Dense(size,activation='tanh'))
+            model.add(Dropout(.5))
+        model.add(Dense(num_classes, activation='softmax'))
+    model.summary()
     return model
-
-    # input = Input(shape=(VOCABULARY_SIZE,), dtype='int32')
-    # embedding = embedding_layer(input)
-    # x = Flatten()(embedding)
-    # x = Activation('relu')(x)
-    # last_layer_width = EMBEDDING_SIZE
-    # for size in hidden_layers:  # Optional additional layers
-    #     x = Dense(size, activation='relu')(x)
-    #     last_layer_width = last_layer_width / 2
-    # preds = Dense(VOCABULARY_SIZE, activation='softmax')(x)
-    # model = Model(input, preds)
-    # return model
