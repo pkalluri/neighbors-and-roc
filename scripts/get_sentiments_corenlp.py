a9951d1a1fd0ea3363b2cbadb95de7996326f5d9
Pratyusha Kalluri <pkalluri@stanford.edu>
2018-03-08 07:41:56 -0800

    Added some fixes to get_sentiments_corenlp to generate a dataset file

--- scripts/get_sentiments_corenlp.py
+++ scripts/get_sentiments_corenlp.py
@@ -11,29 +11,32 @@ import os
 
 # Settings
 data_opts = Data_Defaults()
-data_opts.NUM_STORIES = 1000 # All stories
+data_opts.NUM_STORIES = 100 # All stories
 WRITE_PATH = os.path.join(fixed_settings.GENERATED_DATA_ROOT,'ROC-to-sentiment-coreNLP.txt')
 
 # Sentences
-stories, contexts, completions = util_ROC.get_stories_contexts_completions_and_sentences(data_opts.ROC_FILEPATH, num_stories=data_opts.NUM_STORIES)
+stories, contexts, completions, sentences = util_ROC.get_stories_contexts_completions_and_sentences(data_opts.ROC_FILEPATH, num_stories=data_opts.NUM_STORIES)
 sentences = completions
 
 # Sentiment
 proc = Popen('java -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -stdin', shell=True, stdin=PIPE, stdout=PIPE)
 out,err = proc.communicate('\n'.join(sentences).encode('utf-8'))
-label_words = out.decode('utf-8').split()
+label_words = out.decode('utf-8').split('\n')
+print(label_words)
 
 translate_to_symbols = {}
 translate_to_symbols['Positive'] = '+'
+translate_to_symbols['Very positive'] = '+'
 translate_to_symbols['Negative'] = '-'
+translate_to_symbols['Very negative'] = '-'
 translate_to_symbols['Neutral'] = 'n'
-labels = [translate_to_symbols[label_word] for label_word in label_words]
+labels = [translate_to_symbols[label_word.strip()] for label_word in label_words if label_word.strip()!='']
 
 sentiment_to_simple_responses = {}
 sentiment_to_simple_responses['+']=['That was good.', 'That was great.']
 sentiment_to_simple_responses['-']=['That was bad.', 'That was terrible.']
-dataset = [label+' '+x+' '+random.choice(sentiment_to_simple_responses[label])
-           for (label,x) in zip(labels,X) if label in sentiment_to_simple_responses] # Filter out neutrals
+dataset = [label.strip()+' '+x+' '+random.choice(sentiment_to_simple_responses[label.strip()])
+           for (label,x) in zip(labels,X) if label.strip() in sentiment_to_simple_responses] # Filter out neutrals
 print('\n'.join(dataset), file=open(WRITE_PATH, 'w'))
 
 # with open(os.path.join(fixed_settings.GENERATED_DATA_ROOT,'completions_sentiments-100.txt'), 'w') as f:
